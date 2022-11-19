58adbacd914d8b1cf111110f6bc5d97a7885ea8f
Кирилл Левкович <Kirilllevkovich1904@mail.ru>
2022-11-19 23:11:27 +0800

    ver

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category, Comment, Genre, Review, Title, User
+from .models import Category, Genre, Title
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -24,46 +24,6 @@ class TitlesAdmin(admin.ModelAdmin):
     list_editable = ('name', 'category')
 
 
-class ReviewAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'title', 'text', 'author', 'score', 'pub_date')
-    search_fields = ('text',)
-    empty_value_display = '-пусто-'
-    list_editable = ('text', 'author', 'score')
-
-
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'review', 'author', 'text', 'pub_date')
-    search_fields = ('text',)
-    empty_value_display = '-пусто-'
-    
-@admin.register(User)
-class CustomAdmin(UserAdmin):
-
-    model = User
-    add_fieldsets = (
-        *UserAdmin.add_fieldsets,
-        (None, {'fields': ('email',)}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'bio',)}),
-        ('Permissions', {'fields': ('role',)})
-    )
-
-    fieldsets = (
-        *UserAdmin.fieldsets,
-        ('Permissions', {'fields': ('role',)})
-    )
-
-    list_display = [
-        'username',
-        'email',
-        'first_name',
-        'last_name',
-        'bio',
-        'role',
-    ]
-
-
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Genre, GenreAdmin)
 admin.site.register(Title, TitlesAdmin)
-admin.site.register(Comment, CommentAdmin)
-admin.site.register(Review, ReviewAdmin)
