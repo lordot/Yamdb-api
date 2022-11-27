e7f47545b7a85d37af60c45fad55ae439d6ce8e5
Votoryncev Dmitry <w.dmkvo@gmail.com>
2022-11-27 05:07:43 +0300

    version 2.0

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -72,7 +72,6 @@ class Title(models.Model):
         verbose_name='Описание')
 
     class Meta:
-        ordering = ['name']
         verbose_name = 'Произведение'
         verbose_name_plural = 'Произведения'
 
